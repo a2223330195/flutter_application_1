43b64ff8de531aeb6baea2b6480a612e6ae20700
Gael Antonio Sanchez Garcia <a2223330195@alumnos.uat.edu.mx>
2024-05-09 07:58:22 -0600

    VISTA TAREAS

--- lib/Vista/vista_tareas.dart
+++ lib/Vista/vista_tareas.dart
@@ -12,19 +12,237 @@ class VistaTareas extends StatefulWidget {
 }
 
 class VistaTareasState extends State<VistaTareas> {
-  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Column(
-        children: [
-          // Aquí irá el listado de tareas
-        ],
+      appBar: AppBar(
+        title: const Text('Tareas'),
+        automaticallyImplyLeading: false, // Desactiva el botón de retroceso
+      ),
+      body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        stream: _firestore
+            .collection('Profesores')
+            .doc(widget.clase.profesorId)
+            .collection('Clases')
+            .doc(widget.clase.id)
+            .collection('alumnos')
+            .snapshots(),
+        builder: (context, alumnosSnapshot) {
+          if (!alumnosSnapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          final alumnos = alumnosSnapshot.data!.docs;
+
+          return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+            stream: _firestore
+                .collection('Profesores')
+                .doc(widget.clase.profesorId)
+                .collection('Clases')
+                .doc(widget.clase.id)
+                .collection('tareas')
+                .snapshots(),
+            builder: (context, tareasSnapshot) {
+              if (!tareasSnapshot.hasData) {
+                return const Center(child: CircularProgressIndicator());
+              }
+
+              final tareas = tareasSnapshot.data!.docs;
+
+              return SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: DataTable(
+                  columns: [
+                    const DataColumn(
+                      label: SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Text('Alumno'),
+                      ),
+                    ),
+                    ...tareas.map((tarea) => DataColumn(
+                          label: SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: GestureDetector(
+                              onTap: () {
+                                showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return AlertDialog(
+                                      title: Text(tarea.data()['nombre']),
+                                      content: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Text(
+                                              'Puntaje: ${tarea.data()['puntaje']}'),
+                                          Text(
+                                              'Fecha de entrega: ${tarea.data()['fechaEntrega'].toDate()}'),
+                                        ],
+                                      ),
+                                      actions: [
+                                        TextButton(
+                                          onPressed: () =>
+                                              Navigator.pop(context),
+                                          child: const Text('Cerrar'),
+                                        ),
+                                      ],
+                                    );
+                                  },
+                                );
+                              },
+                              child: Text('T${tareas.indexOf(tarea) + 1}'),
+                            ),
+                          ),
+                        )),
+                  ],
+                  rows: alumnos.map((alumno) {
+                    return DataRow(
+                      cells: [
+                        DataCell(Text(alumno.data()['nombre'])),
+                        ...tareas.map((tarea) => DataCell(
+                              StreamBuilder<
+                                  QuerySnapshot<Map<String, dynamic>>>(
+                                stream: _firestore
+                                    .collection('Profesores')
+                                    .doc(widget.clase.profesorId)
+                                    .collection('Clases')
+                                    .doc(widget.clase.id)
+                                    .collection('tareas')
+                                    .doc(tarea.id)
+                                    .collection('entregas')
+                                    .where('alumnoId', isEqualTo: alumno.id)
+                                    .snapshots(),
+                                builder: (context, entregasSnapshot) {
+                                  if (!entregasSnapshot.hasData) {
+                                    return const CircularProgressIndicator();
+                                  }
+
+                                  final entregas = entregasSnapshot.data!.docs;
+                                  final entregada = entregas.isNotEmpty &&
+                                      entregas.first.data()['entregada'];
+
+                                  return IconButton(
+                                    icon: Icon(
+                                      entregada
+                                          ? Icons.check_circle
+                                          : Icons.cancel,
+                                      color:
+                                          entregada ? Colors.green : Colors.red,
+                                    ),
+                                    onPressed: () async {
+                                      final tareaRef = _firestore
+                                          .collection('Profesores')
+                                          .doc(widget.clase.profesorId)
+                                          .collection('Clases')
+                                          .doc(widget.clase.id)
+                                          .collection('tareas')
+                                          .doc(tarea.id)
+                                          .collection('entregas')
+                                          .doc(alumno.id);
+
+                                      await tareaRef.set({
+                                        'entregada': !entregada,
+                                        'alumnoId': alumno.id
+                                      });
+                                    },
+                                  );
+                                },
+                              ),
+                            )),
+                      ],
+                    );
+                  }).toList(),
+                ),
+              );
+            },
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Aquí se abrirá la vista para agregar una nueva tarea
+        onPressed: () async {
+          final nombre = await showDialog<String>(
+            context: context,
+            builder: (BuildContext context) {
+              final nombreController = TextEditingController();
+              return AlertDialog(
+                title: const Text('Agregar Tarea'),
+                content: TextField(
+                  controller: nombreController,
+                  decoration: const InputDecoration(
+                    hintText: 'Nombre de la tarea',
+                  ),
+                ),
+                actions: [
+                  TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text('Cancelar'),
+                  ),
+                  TextButton(
+                    onPressed: () =>
+                        Navigator.pop(context, nombreController.text),
+                    child: const Text('Aceptar'),
+                  ),
+                ],
+              );
+            },
+          );
+
+          if (nombre != null) {
+            final puntaje = await showDialog<double>(
+              context: context,
+              builder: (BuildContext context) {
+                final puntajeController = TextEditingController();
+                return AlertDialog(
+                  title: const Text('Puntaje de la Tarea'),
+                  content: TextField(
+                    controller: puntajeController,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      hintText: 'Puntaje máximo',
+                    ),
+                  ),
+                  actions: [
+                    TextButton(
+                      onPressed: () => Navigator.pop(context),
+                      child: const Text('Cancelar'),
+                    ),
+                    TextButton(
+                      onPressed: () => Navigator.pop(
+                          context, double.parse(puntajeController.text)),
+                      child: const Text('Aceptar'),
+                    ),
+                  ],
+                );
+              },
+            );
+
+            if (puntaje != null) {
+              final fechaEntrega = await showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime.now(),
+                lastDate: DateTime.now().add(const Duration(days: 365)),
+              );
+              if (fechaEntrega != null) {
+                final tareaRef = _firestore
+                    .collection('Profesores')
+                    .doc(widget.clase.profesorId)
+                    .collection('Clases')
+                    .doc(widget.clase.id)
+                    .collection('tareas')
+                    .doc();
+
+                await tareaRef.set({
+                  'nombre': nombre,
+                  'puntaje': puntaje,
+                  'fechaEntrega': fechaEntrega,
+                });
+              }
+            }
+          }
         },
         child: const Icon(Icons.add),
       ),
